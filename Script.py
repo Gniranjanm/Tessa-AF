7fbab433874333f1826a93f2f3e1e95514106e75
Sarthak (Pricisely) <99407359+sarthakxd16@users.noreply.github.com>
2022-11-08 11:05:58 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,27 +1,31 @@
 class script(object):
-    START_TXT = """𝙷𝙴𝙻𝙾 {},
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
-    HELP_TXT = """𝙷𝙴𝚈 {}
-𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
-    ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>
-✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
-✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
+    START_TXT = """ Hey {} I Am Ｔｅｓｓａ ＡＦ !\nI Am Bot With Open Source Project \nCheck Source Button To Get Source Link\n
+ ✰ I Am A Advanced AutoFilter Bot Made with Python By #ʀᴇǫᴜᴇsᴛʙᴏx1\nEnjoy By Adding To Your Group """
+    HELP_TXT = """Hey {}\nʜᴀᴘᴘʏ ᴜsɪɴɢ ᴛʜɪs ʙᴏᴛ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ✩ ＥＮＪＯＹ ✩
+  𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂"""
+    ABOUT_TXT = """
+✩ ᴄʀᴇᴀᴛᴏʀ <a href=https://t.me/know_sarthak16>✮ ꜱᴀʀᴛʜᴀᴋ ✮</a>
+✧ ʟɪʙʀᴀʀʏ : ᴜᴋɴᴏᴡɴ ⚠︎
+✩ ᴘʏᴛʜᴏɴ : 3.6+
+✧ ᴅᴀᴛᴀʙᴀsᴇ : ‌🇵‌‌🇷‌‌🇮‌‌🇻‌‌🇦‌‌🇹‌‌🇪‌
+✩ sᴇʀᴠᴇʀ : ☁︎︎✈︎ [𝙵𝙻𝙸𝙶𝙷𝚃𝙼𝙾𝙳𝙴]
+✧ ᴠᴇʀsɪᴏɴ : ☃︎𝙑 8.0.5
+✰ ʙᴏᴛ ᴜᴘᴅᴀᴛᴇ : <a href=https://t.me/know_sarthak16>✪ ꜱᴀʀᴛʜᴀᴋ ✪</a>
+✰ sᴏᴜʀᴄᴇ : <a href=https://github.com/sarthakxd16/Tessa-AF>✪ Ｔｅｓｓａ ＡＦ✪</a>
+"""
     SOURCE_TXT = """<b>NOTE:</b>
-- Eva Maria is a open source project. 
-- Source - https://github.com/EvamariaTG/EvaMaria  
+- ＴｅｓｓａＡＦ is a open source project
+- Maintained By #ʀᴇǫᴜᴇsᴛʙᴏx1
 
 <b>DEVS:</b>
-- <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>"""
+☞   <a href=https://t.me/sarthakkale16> ➪ Ｄｅｖ １ ✫</a> \n
+☞   <a href=https://t.me/dev_brahmastra> ➪ Ｄｅｖ ２ ✫</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
-- Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
+- Filter is the feature were users can set automated replies for a particular keyword and ＴＥＳＳＡ ＡＦwill respond whenever a keyword is found the message
 
 <b>NOTE:</b>
-1. eva maria should have admin privillage.
+1. Ｔｅｓｓａ ＡＦhould have admin privillage.
 2. only admins can add filters in a chat.
 3. alert buttons have a limit of 64 characters.
 
@@ -32,15 +36,15 @@ class script(object):
 • /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
     BUTTON_TXT = """Help: <b>Buttons</b>
 
-- Eva Maria Supports both url and alert inline buttons.
+- TessaAF Supports both url and alert inline buttons.
 
 <b>NOTE:</b>
 1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. Eva Maria supports buttons with any telegram media type.
+2. Ｔｅｓｓａ ＡＦsupports buttons with any telegram media type.
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/EvaMariaBot)</code>
+<code>[Button Text](buttonurl:https://t.me/rb1official)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
@@ -67,7 +71,7 @@ class script(object):
     EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
 
 <b>NOTE:</b>
-these are the extra features of Eva Maria
+these are the extra features of Ｔｅｓｓａ ＡＦ
 
 <b>Commands and Usage:</b>
 • /id - <code>get id of a specified user.</code>
@@ -91,17 +95,18 @@ This module only works for my admins
 • /unban  - <code>to unban a user.</code>
 • /channel - <code>to get list of total connected channels</code>
 • /broadcast - <code>to broadcast a message to all users</code>"""
-    STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
-★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
-★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
-    LOG_TEXT_G = """#NewGroup
+    STATUS_TXT = """★  : <code>{}</code>
+★  : <code>{}</code>
+★  : <code>{}</code>
+★  : <code>{}</code> 
+★  : <code>{}</code> 
+★Powered By Ｔｅｓｓａ ＡＦ !"""
+    LOG_TEXT_G = """#NewGroup Of Ｔｅｓｓａ ＡＦ
 Group = {}(<code>{}</code>)
 Total Members = <code>{}</code>
 Added By - {}
 """
-    LOG_TEXT_P = """#NewUser
+    LOG_TEXT_P = """#NewUser Of Ｔｅｓｓａ ＡＦ
 ID - <code>{}</code>
 Name - {}
 """
